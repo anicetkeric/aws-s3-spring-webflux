94f0ad6147304f46924abc59ebf5228e6a1ebe6f
aek <anicetkeric@outlook.com>
2022-12-20 22:06:53 +0100

    init code

---
+++ src/main/java/com/bootlabs/awss3springwebflux/AwsS3SpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.bootlabs.awss3springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SpringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3SpringWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootlabs/awss3springwebflux/AwsS3SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootlabs.awss3springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
